d3ee4e8c9bb6e61ef0e046d429255a665f6769c5
易泽坤 <yzk@qqq.com>
2024-01-09 17:12:48 +0800

    翻页+autoplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Image from 'next/image'
-import type { Metadata } from 'next'
+import Image from "next/image";
+import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: '广州东松科技有限公司',
-  keywords: "激光位移传感器, 激光轮廓传感器, 青波光电,3D扫描,3D线激光,3D相机,3D测量,激光轮廓,轮廓扫描",
-  description: '青波光电专注激光轮廓传感器、激光位移传感器，拥有多年的工业现场实践经验。激光轮廓传感器型号齐全，分辨率高，测量精度高，可用于产品尺寸检测，3D轮廓扫描，焊缝跟踪等应用。激光位移传感器可以非接触、精确测量距离，最高微米级精度、接口丰富可以替代国外同类产品。',
-}
-import LSwiper from '@/components/swiper'
+  title: "广州东松科技有限公司",
+  keywords:
+    "激光位移传感器, 激光轮廓传感器, 青波光电,3D扫描,3D线激光,3D相机,3D测量,激光轮廓,轮廓扫描",
+  description:
+    "青波光电专注激光轮廓传感器、激光位移传感器，拥有多年的工业现场实践经验。激光轮廓传感器型号齐全，分辨率高，测量精度高，可用于产品尺寸检测，3D轮廓扫描，焊缝跟踪等应用。激光位移传感器可以非接触、精确测量距离，最高微米级精度、接口丰富可以替代国外同类产品。",
+};
+import LSwiper from "@/components/swiper";
 export default function App() {
-  return <LSwiper></LSwiper>
+  return (
+    <div style={{ width: "400px", margin: "0 auto" }}>
+      <LSwiper></LSwiper>
+    </div>
+  );
 }
--- src/components/swiper/index.tsx
+++ src/components/swiper/index.tsx
@@ -1,109 +1,192 @@
-"use client"
-import { useEffect, useRef, useState } from "react"
-import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
-let timer: any = null
-export default function LSwiper({ children }: { children?: React.ReactNode, arr?: any[] }) {
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+let timer: any = null;
+export default function LSwiper({
+  children,
+}: {
+  children?: React.ReactNode;
+  arr?: any[];
+}) {
   const arr = [
     "https://images.wallpaperscraft.com/image/single/girl_grass_city_213102_2560x1440.jpg",
     "https://images.wallpaperscraft.com/image/single/girl_pier_river_1129622_2560x1440.jpg",
     "https://images.wallpaperscraft.com/image/single/girl_cute_train_1143644_2560x1440.jpg",
-    "https://images.wallpaperscraft.com/image/single/girl_smile_bob_1136652_2560x1440.jpg"
+    "https://images.wallpaperscraft.com/image/single/girl_smile_bob_1136652_2560x1440.jpg",
   ];
-  const currIndex = useRef(1)
-  const [look, SetLook] = useState(false)
-  const ToggleCurrIndexClick = (payload: number) => {
-    if (look) {
-      return;
-    }
-    SetLook(true)
-    const SwiperContainer: HTMLDivElement = document.querySelector(".l_swiper_container")!
-    const index = currIndex.current + payload
-    SwiperContainer.style.transition = "all .3s"
-    // 加数 下一个
-    currIndex.current = (index);
-    if (index > arr.length) {
-      setTimeout(() => {
-        SwiperContainer.style.transition = "none"
-        currIndex.current = (1)
-      }, 300)
-    } else if (index <= 0) {
-      setTimeout(() => {
-        SwiperContainer.style.transition = "none"
-        currIndex.current = (arr.length)
-      }, 300)
-    }
-    setTimeout(() => {
-      SetLook(false)
-    }, 500)
-  }
 
-  function ControllerAutoPlaySwiperClick() {
-    timer = setInterval(() => {
-      ToggleCurrIndexClick(1)
-    }, 2500)
+  const [outerWidth, SetOuterWidth] = useState(0);
+  function SetOuterWidthWhenWindowResize() {
+    const SwiperContainer: HTMLDivElement =
+      document.querySelector(".l_swiper")!;
+    SetOuterWidth(SwiperContainer.offsetWidth);
+    window.addEventListener("resize", () => {
+      SetOuterWidth(SwiperContainer.offsetWidth);
+    });
     return () => {
-      clearInterval(timer)
-    }
+      window.removeEventListener("resize", () => {});
+    };
   }
   useEffect(() => {
-    const clear = ControllerAutoPlaySwiperClick()
+    const clear = SetOuterWidthWhenWindowResize();
     return () => {
-      clear()
+      clear();
+    };
+  }, []);
+
+  const [transition, SetTransition] = useState(true);
+  const [currIndex, SetCurrIndex] = useState(1);
+
+  const currIndexRef = useRef(currIndex);
+
+  const ToggleCurrIndexClick = async (payload: number) => {
+    console.log("onToggleCurrIndexClick", payload, currIndexRef.current);
+    let next = currIndexRef.current + payload;
+
+    const firstElement: HTMLDivElement =
+      document.querySelector(".l_swiper_img0")!;
+    const lastElement: HTMLDivElement = document.querySelector(
+      `.l_swiper_img${arr.length - 1}`
+    )!;
+
+    if (next === arr.length + 1) {
+      lastElement.style.transform = `translateX(-${outerWidth * arr.length}px)`;
+      next = 0;
+      SetTransition(false);
+      SetCurrIndex(next);
+      currIndexRef.current = next;
+
+      requestAnimationFrame(async () => {
+        SetTransition(true);
+        SetCurrIndex(1);
+        currIndexRef.current = 1;
+
+        await waitContainerTransitionEnd();
+        firstElement.style.transform = `none`;
+        lastElement.style.transform = `none`;
+      });
+    } else if (next === 0) {
+      firstElement.style.transform = `translateX(${outerWidth * arr.length}px)`;
+      next = arr.length + 1;
+      SetTransition(false);
+      SetCurrIndex(next);
+      currIndexRef.current = next;
+
+      requestAnimationFrame(async () => {
+        SetTransition(true);
+        SetCurrIndex(arr.length);
+        currIndexRef.current = arr.length;
+
+        await waitContainerTransitionEnd();
+        firstElement.style.transform = `none`;
+        lastElement.style.transform = `none`;
+      });
+    } else {
+      SetTransition(true);
+      SetCurrIndex(next);
+      currIndexRef.current = next;
     }
-  }, [])
-  return <div className="l_swiper relative">
-    <div
-      style={{
-        width: window.innerWidth * arr.length + "%",
-        transform: `translateX(-${window.innerWidth * currIndex.current}px)`,
-        transition: "all .3s"
-      }}
-      onMouseEnter={() => clearInterval(timer)}
-      onMouseLeave={() => ControllerAutoPlaySwiperClick()}
-      className="clearfix flex l_swiper_container h-96">
-      <img
-        style={{ width: window.innerWidth }}
-        className="w-full object-cover"
-        src={arr[arr.length - 1]}
-        alt="" />
-      {
-        arr.map(src =>
-          <img
-            key={src}
-            style={{ width: window.innerWidth }}
-            className="w-full object-cover"
-            src={src}
-            alt="" />)
+  };
+
+  async function waitContainerTransitionEnd() {
+    const swiperContainerElement: HTMLDivElement = document.querySelector(
+      ".l_swiper_container"
+    )!;
+    await new Promise((resolve) => {
+      const onEnd = () => {
+        console.log("onEnd");
+        resolve("");
+        swiperContainerElement.removeEventListener("transitionend", onEnd);
+      };
+      swiperContainerElement.addEventListener("transitionend", onEnd);
+    });
+  }
+
+  const isMouseEnter = useRef(false);
+  const lastNextTime = useRef(Date.now());
+  const startNext = () => {
+    requestAnimationFrame(() => {
+      if (isMouseEnter.current) return;
+      const now = Date.now();
+      if (now - lastNextTime.current > 3000) {
+        lastNextTime.current = now;
+        ToggleCurrIndexClick(1);
       }
-      <img
-        style={{ width: window.innerWidth }}
-        className="w-full object-cover"
-        src={arr[0]}
-        alt="" />
-    </div>
-    <button className="bg-white/60 rounded-md transition-all hover:bg-white/95 absolute top-1/2 left-0 border block mx-3 px-3 py-3"
-      onClick={() => {
-        ToggleCurrIndexClick(-1)
-      }}>
-      <ArrowLeftIcon></ArrowLeftIcon>
-    </button>
-    <button className="bg-white/60 rounded-md transition-all hover:bg-white/95 absolute top-1/2 right-0 border block mx-3 px-3 py-3"
-      onClick={() => {
-        ToggleCurrIndexClick(1)
-      }}>
-      <ArrowRightIcon></ArrowRightIcon>
-    </button>
-    <ul className="flex mx-auto absolute bottom-4 left-1/2 -translate-x-1/2">
-      {arr.map((item, index) =>
-        <li
-          key={item}
-          onClick={() => ToggleCurrIndexClick(index)}
+      startNext();
+    });
+  };
+
+  useEffect(() => {
+    startNext();
+  }, []); // 空数组表示只在组件挂载时执行一次
+
+  return (
+    <>
+      <div
+        className="l_swiper relative w-full overflow-hidden"
+        onMouseEnter={() => {
+          isMouseEnter.current = true;
+        }}
+        onMouseLeave={() => {
+          isMouseEnter.current = false;
+          startNext();
+        }}
+      >
+        <div
           style={{
-            padding: currIndex.current - 1 == index ? "6px 8px" : "4px 6px",
-            background: currIndex.current - 1 == index ? "rgba(37, 99, 235, 0.8)" : ""
+            width: outerWidth * arr.length,
+            transform: `translateX(${outerWidth * (1 - currIndex)}px)`,
+            transition: transition ? "transform .3s ease-in-out" : "none",
           }}
-          className="bg-white/50 rounded-full mx-2">
-        </li>)}
-    </ul>
-  </div>
+          className="clearfix flex l_swiper_container h-96"
+        >
+          {arr.map((src, index) => (
+            <img
+              key={src}
+              className={`w-full h-full object-cover l_swiper_img${index}`}
+              style={{ width: outerWidth }}
+              src={src}
+              alt=""
+            />
+          ))}
+        </div>
+
+        <button
+          className="bg-white/60 rounded-md transition-all hover:bg-white/95 absolute top-1/2 left-0 border block mx-3 px-3 py-3"
+          onClick={() => {
+            ToggleCurrIndexClick(-1);
+          }}
+        >
+          <ArrowLeftIcon></ArrowLeftIcon>
+        </button>
+        <button
+          className="bg-white/60 rounded-md transition-all hover:bg-white/95 absolute top-1/2 right-0 border block mx-3 px-3 py-3"
+          onClick={() => {
+            ToggleCurrIndexClick(1);
+          }}
+        >
+          <ArrowRightIcon></ArrowRightIcon>
+        </button>
+
+        <ul className="flex mx-auto absolute bottom-4 left-1/2 -translate-x-1/2">
+          {arr.map((item, index) => (
+            <li
+              key={item}
+              onClick={() => ToggleCurrIndexClick(index)}
+              style={{
+                padding: currIndex - 1 == index ? "6px 8px" : "4px 6px",
+                background:
+                  currIndex - 1 == index ? "rgba(37, 99, 235, 0.8)" : "",
+              }}
+              className="bg-white/50 rounded-full mx-2"
+            ></li>
+          ))}
+        </ul>
+      </div>
+      <div>{outerWidth}</div>
+      <div>{currIndex}</div>
+      <div>{transition ? "true" : "false"}</div>
+    </>
+  );
 }
