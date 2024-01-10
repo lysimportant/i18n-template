7746ce1259f59bd2e0d59ad2b0a2ba9c9f08c2a2
lysimportant <lysimportant@outlook.com>
2024-01-10 18:35:17 +0800

    feat: 后台的增加

---
+++ i18n-back/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+  <div>12
+  <RouterView />
+    
+    312
+  </div>
+
+</template>
+
+<style>
+</style>
