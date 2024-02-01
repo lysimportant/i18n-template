60dfddf6d97d5765eddb72bf80b7b05ad594ecac
lysimportant <lysimportant@outlook.com>
2024-02-01 19:45:33 +0800

    msg: 删除Lib

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -3,22 +3,22 @@ import {useEffect, useState} from "react";
 import {ChevronRightIcon, ListBulletIcon} from "@radix-ui/react-icons";
 
 import Link from 'next/link'
-import { useParams, useRouter, usePathname } from "next/navigation";
+import { useParams } from "next/navigation";
 import {useTranslations} from "next-intl";
 
 export default function ShowArticle (){
     const [data, setData] = useState<any[]>([])
-    const { lang } = useParams()
+    const params = useParams()
     const t = useTranslations("technology")
-    const getArticle = async () => {
+    const getArticle = async (lang: string) => {
         // 获取数据
         const res = await fetch(`https://lianghj.top/i18n/article?language=${lang}&title=`)
         const {data} = await res.json()
         setData(data)
     }
     useEffect(() => {
-        getArticle()
-    }, [])
+        getArticle(params.lang as string)
+    }, [params ])
     return  <div className="bg-white shadow-sm text-black border">
         <div className='flex font-semibold items-center gap-2 p-1 bg-gray-300/40'>
             <ListBulletIcon className="font-bule"></ListBulletIcon>
@@ -29,7 +29,7 @@ export default function ShowArticle (){
             {
                 data.map(a => {
                     return <li key={a.ID} >
-                       <Link href={`/${lang}/technology/${a.ID}`} className="flex py-2 items-center cursor-pointer	">
+                       <Link href={`/${params.lang}/technology/${a.ID}`} className="flex py-2 items-center cursor-pointer	">
                            <ChevronRightIcon style={{color: "red"}} className="ml-2"></ChevronRightIcon>
                            <div className="px-2 text-over text-sm">{a.title}</div>
                        </Link>
